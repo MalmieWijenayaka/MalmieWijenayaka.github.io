ee888c29a1797a4820a57a1cfbce2ebf23bc58bf
MalmieWijenayaka <93278@fhss.sjp.ac.lk>
2021-06-18 22:21:45 +0530

    Updates

---
+++ precache-manifest.70996af3a7747c37143251d4030f69fe.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce395129600d26e11dbc2cae13e22569",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5aa1598d6d9302e9146a",
+    "url": "/static/css/main.956fad23.chunk.css"
+  },
+  {
+    "revision": "94833f0d9aaa3be6210f",
+    "url": "/static/js/2.19e99e5d.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.19e99e5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5aa1598d6d9302e9146a",
+    "url": "/static/js/main.8c2dabc9.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "f013614613d180220029e729bd1773bf",
+    "url": "/static/media/hashirshoaeb.f0136146.png"
+  },
+  {
+    "revision": "180e35e387dc47535da36d012b1e5b5e",
+    "url": "/static/media/resume.180e35e3.pdf"
+  }
+]);
